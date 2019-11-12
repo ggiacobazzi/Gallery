376a1f9d6a7438077ec79b14432e78f1cb76cc82
ggiacobazzi <gabrielegiacobazzi@hotmail.it>
2019-11-12 18:10:01 +0100

    added actionlistener. to fix: dispatchEvent

--- Gallery/src/windows/AddWeb.java
+++ Gallery/src/windows/AddWeb.java
@@ -5,46 +5,83 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class AddWeb implements ActionListener {
+public class AddWeb extends JFrame implements ActionListener {
 
-	private String url;
-	private JPanel p;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String urlstring;
+	private JTextField box;
+	private BufferedImage pic;
+	/**
+	 * Class used to create a window when the user tries to add an image from an url
+	 * @param pic
+	 */
 	public AddWeb() {
 		JFrame jf = new JFrame("Add Image from web");
 		jf.setSize(new Dimension(500, 100));
 		jf.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
 		jf.setResizable(false);
-		this.p = new JPanel();
 		JPanel jp = new JPanel();
 		jf.add(jp);
 		JLabel jtf = new JLabel("Insert URL: ");
 		jp.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
 		jp.add(jtf);
-		JTextField box = new JTextField();
+		this.box = new JTextField();
 		box.setPreferredSize(new Dimension(200, 25));
 		jp.add(box);
 		JButton send = new JButton("Enter");
-		send.addActionListener(this);
 		jp.add(send);
+		send.addActionListener(this);
 		jf.setLocationRelativeTo(null);
-		jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		jf.dispatchEvent(new WindowEvent(jf, WindowEvent.WINDOW_CLOSING));
+		//jf.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		jf.setVisible(true);
 		jf.pack();
 	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		//this.url = p.
-		System.out.println("Ciao");
-		
+	/**
+	 * ActionListener 
+	 */
+	public void actionPerformed(ActionEvent e){
+		String choice = e.getActionCommand();
+		if(choice == "Enter") {
+			this.urlstring = box.getText();
+			try {
+				URL url = new URL(urlstring);
+				this.setPic(ImageIO.read(url));
+				System.out.println("Letto URL!");
+			} catch (MalformedURLException e1) {
+				LoadingStatus ls = new LoadingStatus(false);
+				e1.printStackTrace();
+				return;
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			LoadingStatus ls = new LoadingStatus(true);
+		}
+		this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
 	}
+	public BufferedImage getPic() {
+		return pic;
+	}
+	public void setPic(BufferedImage pic) {
+		this.pic = pic;
+	}
+	
+	
 
 }
