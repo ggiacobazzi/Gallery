5a2528da88560e728e1f3182b365cb08548dfd85
ggiacobazzi <gabrielegiacobazzi@hotmail.it>
2019-11-30 17:49:05 +0100

    added metadata to private vars

--- Gallery/src/graphics/LeftPanel.java
+++ Gallery/src/graphics/LeftPanel.java
@@ -3,6 +3,7 @@ package graphics;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
+import java.nio.file.attribute.FileTime;
 
 import javax.swing.JPanel;
 
@@ -11,6 +12,15 @@ public class LeftPanel extends JPanel{
 	/**
 	 * 
 	 */
+	private String name;
+	private String desc;
+	private String date;
+	private String extension;
+	private String path;   
+	private FileTime creation;
+	private FileTime lastaccess;
+	private FileTime lastmodification;
+	
 	private static final long serialVersionUID = 1L;
 
 	public LeftPanel(Color c) {
@@ -21,6 +31,55 @@ public class LeftPanel extends JPanel{
 		this.setLayout(new GridBagLayout());
 		this.setPreferredSize(new Dimension(200,400));
 		this.setMinimumSize(new Dimension(300,400));
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getExtension() {
+		return extension;
+	}
+	public void setExtension(String extension) {
+		this.extension = extension;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public FileTime getCreation() {
+		return creation;
+	}
+	public void setCreation(FileTime creation) {
+		this.creation = creation;
+	}
+	public FileTime getLastaccess() {
+		return lastaccess;
+	}
+	public void setLastaccess(FileTime lastaccess) {
+		this.lastaccess = lastaccess;
+	}
+	public FileTime getLastmodification() {
+		return lastmodification;
+	}
+	public void setLastmodification(FileTime lastmodification) {
+		this.lastmodification = lastmodification;
 	}
 	
 	
